bc522a28e6ab74551913d21986fe66952f96341a
edlessz <42257904+edlessz@users.noreply.github.com>
2025-10-19 00:33:25 -0400

    fix: collisions

--- src/game/components/PlayerController.ts
+++ src/game/components/PlayerController.ts
@@ -14,18 +14,18 @@ class PlayerController extends Component {
 				.getEntities()
 				.flatMap((e) => e.getComponents())
 				.find((c) => c instanceof TileMap) as TileMap) || null;
+
+		this.entity.transform.scale = { x: 1, y: 2 };
 	}
 
 	public update(deltaTime: number): void {
 		const input = this.entity.game.input;
-		if (input.isKeyPressed("ArrowRight")) {
-			this.entity.transform.position.x += this.speed * deltaTime;
-		}
-		if (input.isKeyPressed("ArrowLeft")) {
-			this.entity.transform.position.x -= this.speed * deltaTime;
-		}
+		const direction =
+			(input.isKeyPressed("ArrowRight") ? 1 : 0) -
+			(input.isKeyPressed("ArrowLeft") ? 1 : 0);
+		if (input.isKeyPressed("ArrowUp") && this.grounded()) this.velocity.y = -5;
 
-		this.velocity.x = 0;
+		this.velocity.x = direction * this.speed;
 		this.velocity.y += 0.1;
 
 		this.entity.transform.position.x += this.velocity.x * deltaTime;
@@ -35,6 +35,8 @@ class PlayerController extends Component {
 				this.entity.transform.position.x -= sign * 0.01;
 				this.velocity.x = 0;
 			}
+			this.entity.transform.position.x =
+				Math.round(this.entity.transform.position.x * 2) / 2;
 		}
 		this.entity.transform.position.y += this.velocity.y * deltaTime;
 		if (this.colliding()) {
@@ -43,21 +45,41 @@ class PlayerController extends Component {
 				this.entity.transform.position.y -= sign * 0.01;
 				this.velocity.y = 0;
 			}
+			this.entity.transform.position.y =
+				Math.round(this.entity.transform.position.y * 2) / 2;
 		}
 	}
 
+	public grounded(): boolean {
+		this.entity.transform.position.y += 0.01;
+		const isColliding = this.colliding();
+		this.entity.transform.position.y -= 0.01;
+		return isColliding;
+	}
 	public colliding(): boolean {
 		if (!this.world) return false;
+		const xs = Array.from(
+			{ length: this.entity.transform.scale.x + 1 },
+			(_, i) => i - this.entity.transform.scale.x / 2,
+		);
+		const ys = Array.from(
+			{ length: this.entity.transform.scale.y + 1 },
+			(_, i) => i - this.entity.transform.scale.y / 2,
+		);
 
-		const x = Math.floor(this.entity.transform.position.x);
-		const y = Math.floor(this.entity.transform.position.y);
+		const edgeBoundary = 0.001;
+		xs[0] += edgeBoundary;
+		xs[xs.length - 1] -= edgeBoundary;
+		ys[0] += edgeBoundary;
+		ys[ys.length - 1] -= edgeBoundary;
 
-		return [
-			this.world.getTile(x - 0.5, y - 0.5) > 0,
-			this.world.getTile(x + 0.5, y - 0.5) > 0,
-			this.world.getTile(x - 0.5, y + 0.5) > 0,
-			this.world.getTile(x + 0.5, y + 0.5) > 0,
-		].includes(true);
+		const pairs: Vector2[] = xs.flatMap((x) => ys.map((y) => ({ x, y })));
+
+		const x = this.entity.transform.position.x;
+		const y = this.entity.transform.position.y;
+		return pairs.some(
+			({ x: dx, y: dy }) => (this.world?.getTile(x + dx, y + dy) ?? 0) > 0,
+		);
 	}
 }
 
